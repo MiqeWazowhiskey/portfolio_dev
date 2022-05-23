1adfd63b8121e1447ab98c2cdb2c31e6d365e275
Ali Kagan Yilmaz <kaganylmz12@gmail.com>
2022-05-23 16:42:05 +0300

    Dark mode switch toggle.
    Add dark mode switch toggle.

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Switch } from '@headlessui/react';
 import { useTheme } from 'next-themes';
 const Header = () => {
-  const{theme,setTheme}= useTheme();
+  const{theme,setTheme}= useTheme("dark");
+  const [enabled, setEnabled] = React.useState(false);
   return (
 
     <header className='flex items-center justify-between text-white px-6 py-4 h-1 max-w-8xl mx-auto mt-8 bg-transparent'>
        <span></span> 
-        <button className='px-8 py-2 bg-purple-900 text-white rounded-md font-semibold uppercase'
-                onClick={ () => setTheme(theme === "dark" ? "light" :"dark")}>
-            {theme === "Light" ? "Dark" : "Light"}
-        </button>
+        <Switch
+          checked ={enabled}
+          onChange={() => {  
+          setEnabled(!enabled)
+          setTheme(theme === "dark" ? "light" :"dark")
+          }}
+          className={`${
+            enabled ? 'bg-gray-800 shadow-md shadow-black' : 'bg-gray-600 shadow-gray-500 hover:bg-gray-700 shadow-md'
+          } relative inline-flex h-7 w-12 items-center rounded-full  transition-shadow ease-in-out duration-200`}
+          >
+          <span
+          
+          className={`${enabled ? 'translate-x-6 bg-gray-500' : 'translate-x-1 bg-gray-200'}
+          pointer-events-none inline-block h-5 w-5 transform rounded-full  shadow-lg ring-0 transition duration-200 ease-in-out`}
+      
+        />
+        </Switch> 
+          
+
+        
+
 
     </header>
   )
